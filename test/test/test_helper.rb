cfc8cbb0170d8ad981f24b7753e0c2696b0253b6
Lihan Li <frankieteardrop@gmail.com>
2014-12-30 14:46:14 -0500

    add minitest reporters

--- test/test/test_helper.rb
+++ test/test/test_helper.rb
@@ -2,6 +2,9 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'capybara/rails'
+require "minitest/reporters"
+
+Minitest::Reporters.use!(Minitest::Reporters::SpecReporter.new)
 
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
